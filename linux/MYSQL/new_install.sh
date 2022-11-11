b342be71e959ae51cf331e350f5cd905f37ae80f
we2oxy <45049572+we2oxy@users.noreply.github.com>
2022-11-11 16:15:19 +0000

    update

--- linux/MYSQL/new_install.sh
+++ linux/MYSQL/new_install.sh
@@ -20,31 +20,30 @@ MYSQL_ENV=/usr/local/mysql/env_mysql.sh
 
 function check_list(){
 	#check user
-    [ ! $UID -eq 0 ] && echo 'pls use root execute this scripts' && exit 1
+	[ ! $UID -eq 0 ] && echo 'pls use root execute this scripts' && exit 1
 	#check os_version
 	egrep "CentOS Linux 7" /etc/os-release >/dev/null 2>&1
 	[ ! $? -eq 0 ] && echo 'This Scripts For CentOS7 Use' && exit 2
 	# check_args
-    [ ! $# -eq 1 ]  && echo "Usage:bash mysql_install.sh mysql-5.7.23-el7-x86_64.tar.gz" && exit 3
+	[ ! $# -eq 1 ]	&& echo "Usage:bash mysql_install.sh mysql-5.7.23-el7-x86_64.tar.gz" && exit 3
 }
 
 function init_mysql_user(){
 	groupadd ${MYSQL_GRP} &>/dev/null || sh -c "echo '${MYSQL_GRP} group is exits' && exit 4"
 	id ${MYSQL_USER} &>/dev/null || sh -c "echo 'USER:${MYSQL_USER} or GROUP:${MYSQL_GRP} already exits';exit 5"
-	groupadd ${MYSQL_GRP}
 	useradd -r -g ${MYSQL_GRP} -s /bin/false ${MYSQL_USER} 
 }
 
 function init_mysql(){
-    # init basedir
+	# init basedir
 	tar -zxf $1 -C /usr/local/
-    mv /usr/local/${UNTAR} /usr/local/${INSTALL_VERSION}
-    ln -s /usr/local/${INSTALL_VERSION} ${MYSQL_PREFIX}
+	mv /usr/local/${UNTAR} /usr/local/${INSTALL_VERSION}
+	ln -s /usr/local/${INSTALL_VERSION} ${MYSQL_PREFIX}
 	# init mysql_conf
 	[ -f ${MYSQL_CONF} ] && mv ${MYSQL_CONF} ${MYSQL_CONF}-`date +%F_%T`
 	[ -f /root/.my.cnf ] && mv /root/.my.cnf /root/.my.cnf-`date +%F_%T`
-    echo -e "[client]\nport=3306\ncharacter_set_server=UTF8MB4\nno-auto-rehash" > /root/.my.cnf
-    echo -e "[mysqld]\nport=3306\nbind_address=0.0.0.0\nuser=mysql\ncharacter_set_server=UTF8MB4\nno-auto-rehash\nbasedir=${MYSQL_PREFIX}\ndatadir=${MYSQL_DATADIR}\nsocket=${MYSQL_PREFIX}/mysql.sock\nlog-error=${MYSQL_PREFIX}/error.log\nlog_bin=${MYSQL_PREFIX}/binlog" > ${MYSQL_CONF}
+	echo -e "[client]\nport=3306\ndefault-character-set=UTF8MB4\nno-auto-rehash" > /root/.my.cnf
+	echo -e "[mysqld]\nserver-id=`date +%s`\nport=3306\nbind_address=0.0.0.0\nuser=mysql\ncharacter_set_server=UTF8MB4\nbasedir=${MYSQL_PREFIX}\ndatadir=${MYSQL_DATADIR}\nsocket=${MYSQL_PREFIX}/mysql.sock\nlog-error=${MYSQL_PREFIX}/error.log\nlog_bin=${MYSQL_PREFIX}/binlog" > ${MYSQL_CONF}
 	# init mysql_datadir
 	[ -d ${MYSQL_DATADIR} ] && echo "${MYSQL_DATADIR} already exits!" && exit 6
 	mkdir -p ${MYSQL_DATADIR} ${MYSQL_PREFIX}/binlog
